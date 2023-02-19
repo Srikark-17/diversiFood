d98fcb71d9c1fdb4e1c011b2310def67ee666442
backpack455 <39465356+backpack455@users.noreply.github.com>
2023-02-19 01:31:06 -0500

    Added. config

---
+++ config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+//import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyBJ9ZsjyjFKEzTlIOsjFi0pX1XIUAKDU1Q",
+  authDomain: "diversifood-da44c.firebaseapp.com",
+  projectId: "diversifood-da44c",
+  storageBucket: "diversifood-da44c.appspot.com",
+  messagingSenderId: "436065202140",
+  appId: "1:436065202140:web:ef21d9fa48e3386b7b2919"
+};
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
